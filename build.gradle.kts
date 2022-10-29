7c7d3837fbd386ba343ae5f6bddfd8755e92287f
홍승표/전시개발팀 <edgar.hong@musinsa.com>
2022-10-29 21:34:01 +0900

    실전! 코틀린과 스프링 부트로 도서관리 애플리케이션 개발하기 (Java 프로젝트 리팩토링) 스터디 환경 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("org.springframework.boot") version ("2.6.8")
+    id("io.spring.dependency-management") version ("1.0.11.RELEASE")
     kotlin("jvm") version "1.7.10"
 }
 
@@ -12,6 +14,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     testImplementation(kotlin("test"))
 }
 
