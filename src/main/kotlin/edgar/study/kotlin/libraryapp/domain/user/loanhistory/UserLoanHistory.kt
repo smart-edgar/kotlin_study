bd145376419291f14ee4b22bd248f004f11f3702
edgar.hong <edgar.hong@musinsa.com>
2022-12-12 20:16:07 +0900

    30강. 조금 더 깔끔한 코드로 변경하기

--- src/main/kotlin/edgar/study/kotlin/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ src/main/kotlin/edgar/study/kotlin/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -18,6 +18,9 @@ class UserLoanHistory constructor(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 ) {
+    val isReturn: Boolean
+        get() = this.status == UserLoanStatus.RETURNED
+
     fun doReturn() {
         this.status = UserLoanStatus.RETURNED
     }
--- src/main/kotlin/edgar/study/kotlin/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
+++ src/main/kotlin/edgar/study/kotlin/libraryapp/dto/user/response/UserLoanHistoryResponse.kt
@@ -1,11 +1,33 @@
 package edgar.study.kotlin.libraryapp.dto.user.response
 
+import edgar.study.kotlin.libraryapp.domain.user.User
+import edgar.study.kotlin.libraryapp.domain.user.loanhistory.UserLoanHistory
+
 data class UserLoanHistoryResponse(
     val name: String, //사용자 이름
     val books: List<BookHistoryResponse> //책 대출 현황
-)
+) {
+    companion object {
+        fun of(user: User): UserLoanHistoryResponse {
+            return UserLoanHistoryResponse(
+                user.name,
+                user.userLoanHistories
+                    .map(BookHistoryResponse::of)
+            )
+        }
+    }
+}
 
 data class BookHistoryResponse(
     val name: String, //책 이름
     val isReturn: Boolean //책 반납 여부
-)
+) {
+    companion object {
+        fun of(userLoanHistory: UserLoanHistory): BookHistoryResponse {
+            return BookHistoryResponse(
+                userLoanHistory.bookName,
+                userLoanHistory.isReturn
+            )
+        }
+    }
+}
