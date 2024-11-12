da46df4066cb897d279a929959dc29cecf6e0e78
edgar.hong <edgar.hong@musinsa.com>
2024-11-12 23:37:11 +0900

    이펙티브 코틀린 1장 아이템 08

---
+++ src/main/kotlin/edgar/study/kotlin/effective/item08/fun02.kt
@@ -0,0 +1,32 @@
+package edgar.study.kotlin.effective.item08
+
+//fun process(user: User) {
+//    requireNotNull(user.name)
+//    val context = checkNotNull(context)
+//    val networkService = getNetworkServie(context) ?: throw NoInternetConnection()
+//    networkService.getData { data, userData ->
+//        show(data!!, userData!!)
+//    }
+//}
+
+//fun largestOf(a: Int, b: Int, c: Int, d: Int): Int {
+//    listOf(a, b, c, d).max()!!
+//}
+
+//fun largestOf(vararg nums: Int): Int = nums.max()!!
+
+//class UserControllerTest {
+//    private val dao: UserDao? = null
+//    private val controller: UserController? = null
+//
+//    @BeforeEach
+//    fun init() {
+//        dao = mockk()
+//        controller = UserController(dao!!)
+//    }
+//
+//    @Test
+//    fun test() {
+//        controller!!.doSomething()
+//    }
+//}
