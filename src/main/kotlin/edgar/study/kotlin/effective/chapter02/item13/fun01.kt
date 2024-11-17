7c32699e451e74966172981d71940b8ea4aa5abd
edgar.hong <edgar.hong@musinsa.com>
2024-11-17 20:27:56 +0900

    이펙티브 코틀린 2장 아이템13

---
+++ src/main/kotlin/edgar/study/kotlin/effective/chapter02/item13/fun01.kt
@@ -0,0 +1,9 @@
+package edgar.study.kotlin.effective.chapter02.item13
+
+//fun keyIsCorrect(key: String): Boolean = ...
+//if (!keyIsCorrect(key)) return
+
+//fun verifyKey(key: String): Unit? = ...
+//verifyKey(key) ?: return
+
+//getData()?.let { view.showData(it) } ?: view.showError()
