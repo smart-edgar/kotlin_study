67142325dd2d013411e4cf6f5807dd6b525ed33a
edgar.hong <edgar.hong@musinsa.com>
2024-12-10 20:49:28 +0900

    이펙티브 코틀린 5장 아이템 35

---
+++ src/main/kotlin/edgar/study/kotlin/effective/chapter05/item35/fun01.kt
@@ -0,0 +1,11 @@
+package edgar.study.kotlin.effective.chapter05.item35
+
+//class MyTests : StringSpec({
+//    "length should return size of string" {
+//        "hello".length shouldBe 5
+//    }
+//
+//    "startWith should text for a prefix" {
+//        "world" should startWith("wor")
+//    }
+//})
