c05b8f0e15e8eb976a663d35e161e6b449607576
edgar.hong <edgar.hong@musinsa.com>
2024-12-17 23:56:05 +0900

    이펙티브 코틀린 6장 아이템38

---
+++ src/main/kotlin/edgar/study/kotlin/effective/chapter06/item38/fun01.kt
@@ -0,0 +1,33 @@
+package edgar.study.kotlin.effective.chapter06.item38
+
+//interface OnClick {
+//    fun clicked(view: View)
+//}
+//
+//fun setOnClickListener(listener: OnClick) {
+//
+//}
+//
+//setOnClickListener(object : OnClick {
+//    override fun clicked(view: View) {
+//
+//    }
+//})
+//
+//fun setOnClickListener(listener: (View) -> Unit) {
+//
+//}
+//
+//setOnClickListener { }
+//setOnClickListener(fun(view) { })
+//
+//setOnClickListener(::println)
+//setOnClickListener(this::showUsers)
+//
+//class ClickListener: (View)->Unit {
+//    override fun invoke(view: View) {
+//
+//    }
+//}
+//
+//setOnClickListener(ClickListener())
